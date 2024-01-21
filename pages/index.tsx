457569bad52780f74b1cd66a1896664539e03004
Charlie Maunder <cmaunderc@gmail.com>
2024-01-21 13:25:09 +0000

    changing component case

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from "next";
-import AboutMe from "./AboutMe";
-import LandingPage from "./LandingPage";
-import Work from "./Work";
-import UXResearchDesign from "./UXResearchDesign";
-import Uphoria from "./Uphoria";
-import DFX from "./DFX";
-import Creative from "./Creative";
-import Automotive from "./Automotive";
+import AboutMe from "./components/aboutMe";
+import LandingPage from "./components/landingPage";
+import Work from "./components/work";
+import UXResearchDesign from "./components/uxResearchDesign";
+import Uphoria from "./components/uphoria";
+import DFX from "./components/dfx";
+import Creative from "./components/creative";
+import Automotive from "./components/automotive";
 
 const BasePage: NextPage = () => {
 
