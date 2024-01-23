e3609abcaa8f13cc6629f598ca6ab718dae12e2f
Bessy <bessyhayls96@gmail.com>
2024-01-23 10:49:29 +0000

    Replaced frames with images in the work section

--- pages/components/work.tsx
+++ pages/components/work.tsx
@@ -11,41 +11,21 @@ const Work: NextPage = () => {
     <div className={styles.work}>
       <div className={styles.workChild} />
       <img className={styles.workItem} alt="" src="/vector-2.svg" />
+      <img className={styles.uphoriaBox} alt="" src="/uphoriaBox-1@2x.png" />
       <div className={styles.automotiveDesignBox}>
         <div className={styles.automotive}>
           <div className={styles.automotiveChild} />
-          <img
-            className={styles.automotive21Icon}
-            alt=""
-            src="/automotive2-1@2x.png"
-          />
         </div>
         <div className={styles.automotiveDesignBoxChild} />
       </div>
       <div className={styles.creativeDesignBox}>
         <div className={styles.automotive}>
-          <img
-            className={styles.creativeChild}
-            alt=""
-            src="/rectangle-38.svg"
-          />
-          <img
-            className={styles.savvaki31Icon}
-            alt=""
-            src="/savvaki3-1@2x.png"
-          />
         </div>
         <div className={styles.automotiveDesignBoxChild} />
       </div>
       <div className={styles.uxResearchDesignBox}>
         <div className={styles.automotive}>
           <div className={styles.euphoriaChild} />
-          <img className={styles.icon} alt="" src="/31661652173835--3@2x.png" />
-          <img
-            className={styles.icon1}
-            alt=""
-            src="/31641652173833--3@2x.png"
-          />
         </div>
         <div className={styles.automotiveDesignBoxChild} />
       </div>
